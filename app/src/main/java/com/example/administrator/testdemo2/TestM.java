3fb6b1b629bd46802e3a97d8175cf6abe748f849
zhangtao <553873475@qq.com>
2016-11-24 20:47:10 +0800

    阿凡达发

---
+++ app/src/main/java/com/example/administrator/testdemo2/TestM.java
@@ -0,0 +1,28 @@
+package com.example.administrator.testdemo2;
+
+/**
+ * 作者：Tony
+ * 日期：2016年11月24日
+ * 时间：下午 8:45
+ */
+
+public class TestM {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
