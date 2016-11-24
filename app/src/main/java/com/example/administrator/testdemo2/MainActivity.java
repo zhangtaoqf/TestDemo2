0d384f65896292b4dcc587e0e7e7c08f49aa0ea0
zhangtao <553873475@qq.com>
2016-11-24 20:40:45 +0800

    阿凡达发

--- app/src/main/java/com/example/administrator/testdemo2/MainActivity.java
+++ app/src/main/java/com/example/administrator/testdemo2/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.administrator.testdemo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//注释
 public class MainActivity extends AppCompatActivity {
 
     @Override
